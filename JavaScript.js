dd3694040d68ce515f6a9d6248615772311b9996
DanielM <danielmattsson2010hotmail.com>
2022-12-07 23:31:07 +0100

    v3

--- JavaScript.js
+++ JavaScript.js
@@ -16,8 +16,15 @@ function kontroll(event) {
   if (Name == "" || Andranamn == "" || Mobilnummer == "" || Time == " ") {
     alert("Du måste fylla i alla fält!")
     return false
-  } else {
-    alert(Name + " " + Andranamn + " " + Mobilnummer + " Har bokat " + Time + " " + Radio + " bokad tid")
+  }else if(Number.isInteger(Mobilnummer) === true)
+  {
+    alert("Fyll i ett giltigt mobilnummer!")
+    return false
+  }
+  
+  else {
+    saveEvent()
+    //alert(Name + " " + Andranamn + " " + Mobilnummer + " Har bokat " + Time + " " + Radio + " bokad tid")
   }
 
   createEvent()
@@ -32,14 +39,37 @@ function Efternamnkontroll() {
 }
 
 function createEvent() {
-  const loopForm = document.getElementById("myForm")
-
-  Array.from(loopForm.elements).forEach((element) => {
-    console.log(element.value)
-  })
+  var Name = document.forms["LogInForm"]["ForstaNamn"].value
+  var Andranamn = document.forms["LogInForm"]["SecondName"].value
+  var Mobilnummer = document.forms["LogInForm"]["Nummer"].value
+  var Time = document.forms["LogInForm"]["bookingtime"].value
+  var buttons = document.getElementsByClassName("RadioKnapp") //Hämtar RadioKnapp classerna
+  
+  for (i = 0; i < buttons.length; i++) {
+    //En forloop. i=0, buttons.length som är 3 i detta fall. Loopen kommer köras 3 gånger
+    if (buttons[i].checked) {
+      var Radio = buttons[i].value
+    }
+  }
 
   const eventDiv = document.createElement("p")
   eventDiv.classList.add("Test")
-  eventDiv.textContent = Name
+  eventDiv.textContent = "Du har bokat tennisplanen den " + Time + " i " + Radio
   document.getElementById("Events").appendChild(eventDiv)
+}
+
+function saveEvent () 
+{
+  var Name = document.forms["LogInForm"]["ForstaNamn"].value //Hämtar värdet av "Forstanamn" inuti "LogInForm" som är min <form>
+  var Andranamn = document.forms["LogInForm"]["SecondName"].value
+  var Mobilnummer = document.forms["LogInForm"]["Nummer"].value
+  var Time = document.forms["LogInForm"]["bookingtime"].value
+  var buttons = document.getElementsByClassName("RadioKnapp") //Hämtar RadioKnapp classerna
+  for (i = 0; i < buttons.length; i++) {
+    //En forloop. i=0, buttons.length som är 3 i detta fall. Loopen kommer köras 3 gånger
+    if (buttons[i].checked) {
+      var Radio = buttons[i].value
+    }
+  }
+  return false
 }
