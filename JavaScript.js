0a57e4bb4dec7689904b3c9ba987a6803d3a8552
DanielM <danielmattsson2010hotmail.com>
2022-12-08 18:02:15 +0100

    8/12

--- JavaScript.js
+++ JavaScript.js
@@ -6,6 +6,14 @@ function kontroll(event) {
   var Andranamn = document.forms["LogInForm"]["SecondName"].value
   var Mobilnummer = document.forms["LogInForm"]["Nummer"].value
   var Time = document.forms["LogInForm"]["bookingtime"].value
+  var Bastu = document.forms["LogInForm"]["checkBox"].checked
+  if (Bastu === true)
+  {
+    var bastuBokad = "Extratillägg: Bastu"
+  }
+  else{
+    bastuBokad = "Inget extratillägg"
+  }
   var buttons = document.getElementsByClassName("RadioKnapp") //Hämtar RadioKnapp classerna
   for (i = 0; i < buttons.length; i++) {
     //En forloop. i=0, buttons.length som är 3 i detta fall. Loopen kommer köras 3 gånger
@@ -13,19 +21,30 @@ function kontroll(event) {
       var Radio = buttons[i].value
     }
   }
+
+
+
   if (Name == "" || Andranamn == "" || Mobilnummer == "" || Time == " ") {
     alert("Du måste fylla i alla fält!")
     return false
-  }else if(Number.isInteger(Mobilnummer) === true)
+  }else if(isNaN(Name) === false || isNaN(Andranamn) === false)
+  {
+    alert("Namnen kan inte innehålla siffror")
+    return false
+  }
+  else if(isNaN(Mobilnummer))//Checkar ifall Mobilnummer är INT, 'isNaN' retunerar antingen true eller false
   {
     alert("Fyll i ett giltigt mobilnummer!")
     return false
   }
+
+
   
-  else {
-    saveEvent()
-    //alert(Name + " " + Andranamn + " " + Mobilnummer + " Har bokat " + Time + " " + Radio + " bokad tid")
+
+  else{
+    alert(Name + " " + Andranamn + " " + Mobilnummer + " Har bokat " + Time + " " + Radio + " bokad tid, " + bastuBokad)
   }
+
 
   createEvent()
 }
@@ -43,6 +62,14 @@ function createEvent() {
   var Andranamn = document.forms["LogInForm"]["SecondName"].value
   var Mobilnummer = document.forms["LogInForm"]["Nummer"].value
   var Time = document.forms["LogInForm"]["bookingtime"].value
+  var Bastu = document.forms["LogInForm"]["checkBox"].checked
+  if (Bastu === true)
+  {
+    var bastuBokad = "Extratillägg: Bastu"
+  }
+  else{
+    bastuBokad = "Inget extratillägg"
+  }
   var buttons = document.getElementsByClassName("RadioKnapp") //Hämtar RadioKnapp classerna
   
   for (i = 0; i < buttons.length; i++) {
@@ -54,22 +81,7 @@ function createEvent() {
 
   const eventDiv = document.createElement("p")
   eventDiv.classList.add("Test")
-  eventDiv.textContent = "Du har bokat tennisplanen den " + Time + " i " + Radio
+  eventDiv.textContent = "Du har bokat tennisplanen den " + Time + " i " + Radio + " " + bastuBokad
   document.getElementById("Events").appendChild(eventDiv)
 }
 
-function saveEvent () 
-{
-  var Name = document.forms["LogInForm"]["ForstaNamn"].value //Hämtar värdet av "Forstanamn" inuti "LogInForm" som är min <form>
-  var Andranamn = document.forms["LogInForm"]["SecondName"].value
-  var Mobilnummer = document.forms["LogInForm"]["Nummer"].value
-  var Time = document.forms["LogInForm"]["bookingtime"].value
-  var buttons = document.getElementsByClassName("RadioKnapp") //Hämtar RadioKnapp classerna
-  for (i = 0; i < buttons.length; i++) {
-    //En forloop. i=0, buttons.length som är 3 i detta fall. Loopen kommer köras 3 gånger
-    if (buttons[i].checked) {
-      var Radio = buttons[i].value
-    }
-  }
-  return false
-}
